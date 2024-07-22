756d86db44e98b0a3aa9722570553b42dcf7a1c7
nmorrone <n.morrone96@gmail.com>
2024-07-22 16:11:40 +0200

    snack-02 start

---
+++ JS/snack-02.js
@@ -0,0 +1,14 @@
+console.log('Snack-02 - Iniziamo')
+
+/* l'utente inserisce 2 parole e il software
+stampa la parola che contiene più caratteri */
+
+//definisco le variabili delle parole che inserirà l'utente
+
+const word1 = prompt('inserisci una parola a piacere')
+
+const word2 = prompt('inserisci un altra parola a piacere')
+
+//verifico funzionamento
+
+console.log(word1, word2)
