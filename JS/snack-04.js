9ffe90af71b5a3094753a1ed2d9b37fc6c517ab7
nmorrone <n.morrone96@gmail.com>
2024-07-22 22:02:29 +0200

    array-setted

---
+++ JS/snack-04.js
@@ -0,0 +1,6 @@
+console.log('Party: Il Grande Gatsby')
+
+/* definisco l'Array nel quale inserirò
+la lista completa dei nomi degli invitati */
+
+let invitati = ['Gianni', 'Marco', 'Laura', 'Sara', 'Nicola', 'Gabriel', 'Tommaso', 'Carmela', 'Amalia', 'Lucia', 'Mattia', 'Sandra', 'Federico']  //strings array
