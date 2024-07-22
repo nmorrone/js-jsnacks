14822ec74d9e454df9d74655ce4d6d68744dbc4d
nmorrone <n.morrone96@gmail.com>
2024-07-22 16:45:29 +0200

    snack-02 complete

--- JS/snack-02.js
+++ JS/snack-02.js
@@ -3,12 +3,32 @@ console.log('Snack-02 - Iniziamo')
 /* l'utente inserisce 2 parole e il software
 stampa la parola che contiene più caratteri */
 
-//definisco le variabili delle parole che inserirà l'utente
+//definisco le variabili delle parole che verranno inserite dall'utente
 
-const word1 = prompt('inserisci una parola a piacere')
+const parola1 = prompt('Inserisci una parola a caso')
 
-const word2 = prompt('inserisci un altra parola a piacere')
+const parola2 = prompt('Inserisci una seconda parola')
+
+//acquisisco l'elemento html nel quale successivamente stamperò
+
+const printElement = document.getElementById('bigger')
 
 //verifico funzionamento
 
-console.log(word1, word2)
+console.log(parola1, parola2)
+
+
+/* inserisco le condizioni per stampare la parola più corta prima 
+di quella più lunga usando length per far leggere a JS la lunghezza 
+dei caratteri delle parole inserite dall'utente */
+
+if (parola1.length > parola2.length) {
+
+    printElement.innerHTML = parola2 + ' ' + parola1
+
+}
+
+else {
+
+    printElement.innerHTML = parola1 + ' ' + parola2
+}
