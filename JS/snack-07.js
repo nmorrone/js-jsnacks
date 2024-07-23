da45ea539691aa251d4bbcb9fa23675009ec3a7c
nmorrone <n.morrone96@gmail.com>
2024-07-23 11:13:01 +0200

    snack-07

--- JS/snack-07.js
+++ JS/snack-07.js
@@ -3,20 +3,41 @@ console.log('Snack-07 - Iniziamo')
 /* Scrivo programma che stampa tabellina del 2
 fino al raggiungimento del numero 1000 */
 
-//1000/2 = 500 (numero di interazioni che mi serviranno)
-
 //collego elemento HTML che mi servirà
 
-let timetableElement = document.getElementById('timetable')
+let timeTableElement = document.getElementById('timetable')
 
 //definisco la variabile che aumenterà di valore +2/each
+//ovvero quella della tabellina del 2
 
-let two = 0
+let twoTimeTable = 0
+
+//definisco un Array che mi servirà per contenere l'elenco
+let tabellina = []
+
 
 /* apro il ciclo FOR con 500 interazioni che sono 
 quelle che mi serviranno per il programma */
 
+//2*500=100, numero da raggiungere
+
 for (i = 0; i < 500; i++) {
+
+    //inserisco la condizione di aumento del valore della variabile
+    twoTimeTable = twoTimeTable + 2
+
+    //verifico funzionamento
+    console.log(twoTimeTable)
+
+    //inserisco valore all'interno dell'Array
+    tabellina.push(' ' + twoTimeTable + ' ')
 
 
 }
+
+//stampo nella pagina HTML la tabellina completa
+timeTableElement.innerHTML = tabellina
+
+
+
+
