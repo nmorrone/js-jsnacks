8a9fd9ffa0d2798d6678b5e2ca8f163feb6158af
nmorrone <n.morrone96@gmail.com>
2024-07-23 10:41:56 +0200

    snack-06-variabili-definite

---
+++ JS/snack-07.js
@@ -0,0 +1,22 @@
+console.log('Snack-07 - Iniziamo')
+
+/* Scrivo programma che stampa tabellina del 2
+fino al raggiungimento del numero 1000 */
+
+//1000/2 = 500 (numero di interazioni che mi serviranno)
+
+//collego elemento HTML che mi servirà
+
+let timetableElement = document.getElementById('timetable')
+
+//definisco la variabile che aumenterà di valore +2/each
+
+let two = 0
+
+/* apro il ciclo FOR con 500 interazioni che sono 
+quelle che mi serviranno per il programma */
+
+for (i = 0; i < 500; i++) {
+
+
+}
