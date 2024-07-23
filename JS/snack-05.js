52b47a77d2f2f53e0008117f0bf4313c197331a4
nmorrone <n.morrone96@gmail.com>
2024-07-23 09:30:16 +0200

    snack-05

--- JS/snack-05.js
+++ JS/snack-05.js
@@ -1,1 +1,36 @@
 console.log('Snack-05 - Iniziamo')
+
+/* crea un Array vuoti
+chiedi all'utente 6 volte di inserire un numero
+se il numero è dispari viene aggiunto all'interno dell'Array */
+
+//recupero 
+
+let numeri = []
+
+const numbersElement = document.getElementById('numbers')
+
+//apro il ciclo con 6 interazioni
+
+for (i = 0; i < 6; i++) {
+
+    //chiedo all'utente di inserire un numero casuale
+    let inputNumber = parseInt(prompt('Inserisci un numero'))
+
+    //controllo se il numero è dispari
+    resto = inputNumber % 2
+
+    /*assegno condizione per la quale se il numero è
+    dispari viene aggiunto nell'Array */
+    if (resto == 1) {
+
+        numeri.push(inputNumber)
+    }
+
+}
+
+//stampo il risultato in console
+console.log(numeri)
+
+//stampo sulla pagina HTML
+numbersElement.innerHTML = numeri
